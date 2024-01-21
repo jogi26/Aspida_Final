e80a26842669aa0a01eb48b2a5be0ef4e84bf438
Jaskaran Shekhon <jaskaranshekhon@gmail.com>
2024-01-21 02:05:51 -0800

    Zoomed in, locations added, everything good

--- app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -37,74 +37,80 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
+
+
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
         val mainLocation = LatLng(33.973977, -117.328114)
-        mMap.addMarker(MarkerOptions().position(mainLocation).title("UCR"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(mainLocation))
+        mMap.run {
+            addMarker(MarkerOptions().position(mainLocation).title("UCR"))
+            moveCamera(CameraUpdateFactory.newLatLngZoom(mainLocation, 15f))
+        }
 
-        val box1= LatLng (-117.3245686,33.973691)
-        val box2= LatLng (-117.3230413,33.9737365)
-        val box3= LatLng (-117.3221182,33.9743244)
-        val box4= LatLng (-117.320512,33.9745171)
-        val box5= LatLng (-117.3195664,33.9753148)
-        val box6= LatLng (-117.3183505,33.9753028)
-        val box7= LatLng (-117.3221386,33.9756371)
-        val box8= LatLng (-117.321272,33.9755388)
-        val box9= LatLng (-117.3190285,33.9772263)
-        val box10= LatLng (-117.3203521,33.976798)
-        val box11= LatLng (-117.3237709,33.9768521)
-        val box12= LatLng (-117.3243224,33.9762503)
-        val box13= LatLng (-117.3258118,33.976633)
-        val box14= LatLng (-117.3228877,33.9786475)
-        val box15= LatLng (-117.3218503,33.9786629)
-        val box16= LatLng (-117.3207768,33.9778799)
-        val box17= LatLng (-117.320362,33.978019)
-        val box18= LatLng (-117.3207381,33.9786403)
-        val box19= LatLng (-117.318877,33.9792846)
-        val box20= LatLng (-117.322259,33.9796985)
-        val box21= LatLng (-117.3234647,33.9796514)
-        val box22= LatLng (-117.3239476,33.9792059)
-        val box23= LatLng (-117.3246666,33.9789699)
-        val box24= LatLng (-117.3247393,33.978394)
-        val box25= LatLng (-117.3250435,33.9790997)
-        val box26= LatLng (-117.32556,33.9796291)
-        val box27= LatLng (-117.3256797,33.9790607)
-        val box28= LatLng (-117.3249889,33.9775694)
-        val box29= LatLng (-117.3256375,33.9775189)
-        val box30= LatLng (-117.3234121,33.9749422)
-        val box31= LatLng (-117.325205,33.9750865)
-        val box32= LatLng (-117.3229749,33.9711556)
-        val box33= LatLng (-117.3259346,33.9702871)
-        val box34= LatLng (-117.3283329,33.9704721)
-        val box35= LatLng (-117.3269983,33.9694428)
-        val box36= LatLng (-117.3259031,33.9678377)
-        val box37= LatLng (-117.3306273,33.9696535)
-        val box38= LatLng (-117.3308921,33.9706681)
-        val box39= LatLng (-117.3319615,33.9703305)
-        val box40= LatLng (-117.3336224,33.9694471)
-        val box41= LatLng (-117.3314972,33.9687061)
-        val box42= LatLng (-117.3294293,33.9709868)
-        val box43= LatLng (-117.3371532,33.9731675)
-        val box44= LatLng (-117.3361891,33.9742848)
-        val box45= LatLng (-117.336725,33.9753572)
-        val box46= LatLng (-117.3316699,33.9748853)
-        val box47= LatLng (-117.3298818,33.9750984)
-        val box48= LatLng (-117.3279178,33.9764774)
-        val box49= LatLng (-117.327428,33.976549)
-        val box50= LatLng (-117.3276021,33.9757856)
-        val box51= LatLng (-117.3276225,33.9748987)
-        val box52= LatLng (-117.3278389,33.973026)
-        val box53= LatLng (-117.3266957,33.9718982)
-        val box54= LatLng (-117.3309412,33.978894)
-        val box55= LatLng (-117.3319282,33.9786669)
-        val box56= LatLng (-117.333828,33.9777098)
-        val box57= LatLng (-117.3334152,33.9784272)
-        val box58= LatLng (-117.3328093,33.9792151)
-
-        //adding points now
+        val box1= LatLng (33.973691,-117.3245686)
+        val box2= LatLng (33.9737365,-117.3230413)
+        val box3= LatLng (33.9743244,-117.3221182)
+        val box4= LatLng (33.9745171,-117.320512)
+        val box5= LatLng (33.9753148,-117.3195664)
+        val box6= LatLng (33.9753028,-117.3183505)
+        val box7= LatLng (33.9756371,-117.3221386)
+        val box8= LatLng (33.9755388,-117.321272)
+        val box9= LatLng (33.9772263,-117.3190285)
+        val box10= LatLng (33.976798,-117.3203521)
+        val box11= LatLng (33.9768521,-117.3237709)
+        val box12= LatLng (33.9762503,-117.3243224)
+        val box13= LatLng (33.976633,-117.3258118)
+        val box14= LatLng (33.9786475,-117.3228877)
+        val box15= LatLng (33.9786629,-117.3218503)
+        val box16= LatLng (33.9778799,-117.3207768)
+        val box17= LatLng (33.978019,-117.320362)
+        val box18= LatLng (33.9786403,-117.3207381)
+        val box19= LatLng (33.9792846,-117.318877)
+        val box20= LatLng (33.9796985,-117.322259)
+        val box21= LatLng (33.9796514,-117.3234647)
+        val box22= LatLng (33.9792059,-117.3239476)
+        val box23= LatLng (33.9789699,-117.3246666)
+        val box24= LatLng (33.978394,-117.3247393)
+        val box25= LatLng (33.9790997,-117.3250435)
+        val box26= LatLng (33.9796291,-117.32556)
+        val box27= LatLng (33.9790607,-117.3256797)
+        val box28= LatLng (33.9775694,-117.3249889)
+        val box29= LatLng (33.9775189,-117.3256375)
+        val box30= LatLng (33.9749422,-117.3234121)
+        val box31= LatLng (33.9750865,-117.325205)
+        val box32= LatLng (33.9711556,-117.3229749)
+        val box33= LatLng (33.9702871,-117.3259346)
+        val box34= LatLng (33.9704721,-117.3283329)
+        val box35= LatLng (33.9694428,-117.3269983)
+        val box36= LatLng (33.9678377,-117.3259031)
+        val box37= LatLng (33.9696535,-117.3306273)
+        val box38= LatLng (33.9706681,-117.3308921)
+        val box39= LatLng (33.9703305,-117.3319615)
+        val box40= LatLng (33.9694471,-117.3336224)
+        val box41= LatLng (33.9687061,-117.3314972)
+        val box42= LatLng (33.9709868,-117.3294293)
+        val box43= LatLng (33.9731675,-117.3371532)
+        val box44= LatLng (33.9742848,-117.3361891)
+        val box45= LatLng (33.9753572,-117.336725)
+        val box46= LatLng (33.9748853,-117.3316699)
+        val box47= LatLng (33.9750984,-117.3298818)
+        val box48= LatLng (33.9764774,-117.3279178)
+        val box49= LatLng (33.976549,-117.327428)
+        val box50= LatLng (33.9757856,-117.3276021)
+        val box51= LatLng (33.9748987,-117.3276225)
+        val box52= LatLng (33.973026,-117.3278389)
+        val box53= LatLng (33.9718982,-117.3266957)
+        val box54= LatLng (33.978894,-117.3309412)
+        val box55= LatLng (33.9786669,-117.3319282)
+        val box56= LatLng (33.9777098,-117.333828)
+        val box57= LatLng (33.9784272,-117.3334152)
+        val box58= LatLng (33.9792151,-117.3328093)
 
 
         mMap.addMarker(MarkerOptions().position(box1).title("Blue Box" ))
@@ -166,5 +172,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(box57).title("Blue Box" ))
         mMap.addMarker(MarkerOptions().position(box58).title("Blue Box" ))
 
+
+
+
+
     }
+
+
+
+
 }
